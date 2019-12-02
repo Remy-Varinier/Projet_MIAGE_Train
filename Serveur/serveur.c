eca59c554f269303fc2ff3914be8c4ddd4a29e24
Stéphanie <stephanie.gourdon05@hotmail.fr>
2019-12-02 10:17:32 +0100

    ajout structures et data

--- Serveur/serveur.c
+++ Serveur/serveur.c
@@ -10,7 +10,26 @@
 #include <arpa/inet.h>
 #include <sys/uio.h>
 
-#define MAX 100
+#define MAX 256               /* Nombre maximum de caractères lus */
+#define MAX_CAR 256		      /* Nombre maximum de caractères possible*/
+
+struct train{ 
+	int num_train;            /* numéro du train */
+	char ville_dep[MAX_CAR];  /* ville de départ */
+	char ville_arr[MAX_CAR];  /* ville d'arrivée */
+	struct horaire h_depart;  /* heure de départ */
+	struct horaire h_arrivee; /* heure d'arrivée */
+	float prix;               /* prix usuel */
+	char option[5]            /* champ optionnel:
+								 REDUC si le train bénéficie d'une remise de 20%
+								 SUPPL si le train fait l'objet d'un supplément de 10%
+							   */
+};
+
+struct horaire{
+	short heure; 
+	short minute; 
+};
 
 int main(int argc,char *argv[]){ 
 	/*int ecoute = socket(AF_INET, SOCK_STREAM, 0);
