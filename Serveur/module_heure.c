7cd9e65759ece94218eb784a2b58ce9556d0eef2
Stéphanie <stephanie.gourdon05@hotmail.fr>
2019-12-03 10:59:22 +0100

    changement repertoire

--- Serveur/module_heure.c
+++ Serveur/module_heure.c
@@ -9,3 +9,11 @@ int heureVersMinutes(struct Horaire h){
 	int minutes = ((h.heure)*60) + (h.minutes);
 	return minutes;
 }
+
+int compTemps(int h1, h2){
+	if(h1<h2)
+		return 1;
+	if(h1>h2)
+		return -1;
+	return 0;
+}
